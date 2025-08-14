c111784788db4071d7e16cc75e76a98865e5553c
ricespiceusa <ricespiceusa@gmail.com>
2025-08-15 01:07:30 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/rice-and-spice-website/' : '/',
+  base: '/',
   plugins: [vue()],
   server: {
     port: 3000,
